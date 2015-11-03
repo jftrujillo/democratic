27d343457a439b8c68f8709cfa4b75ee27b57456
manuelframirez <manuel.ramirez.v@gmail.com>
2015-11-03 18:37:38 -0500

    prueba git

--- app/src/main/java/com/kcumendigital/democratic/LoginActivity.java
+++ app/src/main/java/com/kcumendigital/democratic/LoginActivity.java
@@ -7,12 +7,14 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.TextView;
 
 public class LoginActivity extends AppCompatActivity implements View.OnClickListener {
 
     TextInputLayout email, pass;
     Button btnRegistro;
     FloatingActionButton ingresar;
+    TextView fredy;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
