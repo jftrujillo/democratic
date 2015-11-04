fb9edf2841d1ef3b0807eb6631694283cc6dc032
Jhon Trujillo <jftrujillo@unicauca.edu.co>
2015-11-04 15:38:13 -0500

    mas arreglos de interfaces

--- app/src/main/java/com/kcumendigital/democratic/Fragments/NumberFragmetFragment.java
+++ app/src/main/java/com/kcumendigital/democratic/Fragments/NumberFragmetFragment.java
@@ -44,10 +44,25 @@ public class NumberFragmetFragment extends android.support.v4.app.Fragment {
         // Inflate the layout for this fragment
         View v = inflater.inflate(R.layout.fragment_number_fragmet, container, false);
         TextView txt = (TextView) v.findViewById(R.id.numberPage);
+        ImageView img_bacground = (ImageView) v.findViewById(R.id.bacground_pager);
         if (TYPE_lIST == PagerAdpater.TYPE_DISCUSION) {
             discussion = ColletionsStatics.getDataDiscusion().get(position);
             txt.setText(discussion.getTitle());
             ImageView img = (ImageView) v.findViewById(R.id.img);
+            String categoria = discussion.getCategory();
+            if (categoria.equals("Salud")){
+                Picasso.with(getActivity()).load(R.drawable.bg_salud).into(img_bacground);
+            }
+
+            if (categoria.equals("Gobierno")){
+                Picasso.with(getActivity()).load(R.drawable.bg_ciudad).into(img_bacground);
+            }
+
+            if (categoria.equals("Educación")){
+                Picasso.with(getActivity()).load(R.drawable.ic_democratic_educacion).into(img_bacground);
+
+            }
+
             if (position == 0) {
                 Picasso.with(getActivity()).load(R.drawable.dots).into(img);
             }
@@ -61,6 +76,20 @@ public class NumberFragmetFragment extends android.support.v4.app.Fragment {
             survey = ColletionsStatics.getDataSurvey().get(position);
             txt.setText(survey.getTitle());
             ImageView img = (ImageView) v.findViewById(R.id.img);
+            String categoria = survey.getCategory();
+            if (categoria.equals("Salud")){
+                Picasso.with(getActivity()).load(R.drawable.bg_salud).into(img_bacground);
+            }
+
+            if (categoria.equals("Gobierno")){
+                Picasso.with(getActivity()).load(R.drawable.bg_ciudad).into(img_bacground);
+            }
+
+            if (categoria.equals("Educación")){
+                Picasso.with(getActivity()).load(R.drawable.ic_democratic_educacion).into(img_bacground);
+
+            }
+
             if (position == 0) {
                 Picasso.with(getActivity()).load(R.drawable.dots).into(img);
             }
