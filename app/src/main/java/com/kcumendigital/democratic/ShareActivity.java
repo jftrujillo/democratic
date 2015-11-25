a0f3ddc3c54b642f79f42a3c388bfe7e4bf9f740
manuelframirez <manuel.ramirez.v@gmail.com>
2015-11-25 12:22:43 -0500

    commit lasagna

--- app/src/main/java/com/kcumendigital/democratic/ShareActivity.java
+++ app/src/main/java/com/kcumendigital/democratic/ShareActivity.java
@@ -2,12 +2,32 @@ package com.kcumendigital.democratic;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
-public class ShareActivity extends AppCompatActivity {
+import com.kcumendigital.democratic.library.Techniques;
+import com.kcumendigital.democratic.library.YoYo;
+import com.kcumendigital.democratic.library.attention.TadaAnimator;
 
+public class ShareActivity extends AppCompatActivity implements View.OnClickListener {
+
+    Button share;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_share);
+
+        share = (Button) findViewById(R.id.share);
+        share.setOnClickListener(this);
+
+    }
+
+    @Override
+    public void onClick(View view) {
+        switch (view.getId()){
+            case R.id.share:
+                YoYo.with(Techniques.Pulse).duration(700).playOn(findViewById(R.id.share));
+                break;
+        }
     }
 }
