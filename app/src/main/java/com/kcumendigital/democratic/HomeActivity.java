5245c0335bdca95d87574476580591a2dc08fb4e
Jhon Trujillo <jftrujillo@unicauca.edu.co>
2015-11-26 12:26:16 -0500

    intento uno de animacion

--- app/src/main/java/com/kcumendigital/democratic/HomeActivity.java
+++ app/src/main/java/com/kcumendigital/democratic/HomeActivity.java
@@ -1,7 +1,13 @@
 package com.kcumendigital.democratic;
 
+import android.animation.Animator;
+import android.animation.AnimatorListenerAdapter;
+import android.animation.AnimatorSet;
+import android.animation.ObjectAnimator;
 import android.app.SearchManager;
 import android.content.Intent;
+import android.graphics.Point;
+import android.graphics.Rect;
 import android.os.Bundle;
 import android.support.design.widget.NavigationView;
 import android.support.design.widget.TabLayout;
@@ -18,6 +24,7 @@ import android.view.Menu;
 import android.view.MenuInflater;
 import android.view.MenuItem;
 import android.view.View;
+import android.view.animation.DecelerateInterpolator;
 import android.widget.ImageView;
 import android.widget.TextView;
 import android.widget.Toast;
@@ -44,7 +51,12 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class HomeActivity extends AppCompatActivity implements NavigationView.OnNavigationItemSelectedListener, View.OnClickListener, SurveyListAdapter.OnItemClickListenerSurvey, SearchView.OnQueryTextListener, MenuItemCompat.OnActionExpandListener {
+    private Animator mCurrentAnimator;
 
+    // The system "short" animation time duration, in milliseconds. This
+    // duration is ideal for subtle animations or animations that occur
+    // very frequently.
+    private int mShortAnimationDuration;
 
 
     DiscussionHomeFragment fragment;
@@ -83,23 +95,24 @@ public class HomeActivity extends AppCompatActivity implements NavigationView.On
         fragment = new DiscussionHomeFragment();
         drawer = (DrawerLayout) findViewById(R.id.drawer);
         nav = (NavigationView) findViewById(R.id.nav);
-        nav.getMenu().setGroupVisible(R.id.persona_menu_nav,false);
-        nav.getMenu().setGroupVisible(R.id.secciones_menu_nav,true);
+        nav.getMenu().setGroupVisible(R.id.persona_menu_nav, false);
+        nav.getMenu().setGroupVisible(R.id.secciones_menu_nav, true);
 
-        ImageView imm_nav = (ImageView) nav.findViewById(R.id.img_user_nav);
+        final ImageView imm_nav = (ImageView) nav.findViewById(R.id.img_user_nav);
         imm_nav.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                if (alreadyTouchedDogue == false){
-                Toast.makeText(getApplicationContext(),"cuidado con dogue",Toast.LENGTH_SHORT).show();
-                nav.getMenu().setGroupVisible(R.id.persona_menu_nav,true);
-                nav.getMenu().setGroupVisible(R.id.secciones_menu_nav,false);
-                alreadyTouchedDogue = true;
-            }
-                else {
-                    nav.getMenu().setGroupVisible(R.id.persona_menu_nav,false);
-                    nav.getMenu().setGroupVisible(R.id.secciones_menu_nav,true);
-                    alreadyTouchedDogue =false;
+                zoomImageFromThumb(imm_nav,R.drawable.democraticmarca);
+
+                if (alreadyTouchedDogue == false) {
+                    Toast.makeText(getApplicationContext(), "cuidado con dogue", Toast.LENGTH_SHORT).show();
+                    nav.getMenu().setGroupVisible(R.id.persona_menu_nav, true);
+                    nav.getMenu().setGroupVisible(R.id.secciones_menu_nav, false);
+                    alreadyTouchedDogue = true;
+                } else {
+                    nav.getMenu().setGroupVisible(R.id.persona_menu_nav, false);
+                    nav.getMenu().setGroupVisible(R.id.secciones_menu_nav, true);
+                    alreadyTouchedDogue = false;
                 }
             }
 
@@ -141,9 +154,9 @@ public class HomeActivity extends AppCompatActivity implements NavigationView.On
         tabLayout.setTabsFromPagerAdapter(adapter);
         tabLayout.setupWithViewPager(viewPager);
 
-        if (getResources().getBoolean(R.bool.port) == true){
+        if (getResources().getBoolean(R.bool.port) == true) {
             getSupportActionBar().show();
-        }else{
+        } else {
             getSupportActionBar().hide();
         }
 
@@ -162,7 +175,6 @@ public class HomeActivity extends AppCompatActivity implements NavigationView.On
         MenuItemCompat.setOnActionExpandListener(menu.findItem(R.id.navigate), this);
         return true;
     }
-
 
 
     @Override
@@ -189,7 +201,7 @@ public class HomeActivity extends AppCompatActivity implements NavigationView.On
         filtro = menuItem.getTitle().toString();
         getSupportActionBar().setTitle(filtro);
 
-        switch(menuItem.getItemId()){
+        switch (menuItem.getItemId()) {
             case R.id.nav_home:
                 query = null;
                 break;
@@ -240,7 +252,7 @@ public class HomeActivity extends AppCompatActivity implements NavigationView.On
                 startActivity(new Intent(this, CreateBoardDiscussion_activity.class));
                 break;
             case R.id.navigate:
-                Log.i("navigate","true");
+                Log.i("navigate", "true");
                 break;
 
         }
@@ -254,7 +266,7 @@ public class HomeActivity extends AppCompatActivity implements NavigationView.On
 
     @Override
     public boolean onQueryTextSubmit(String queryString) {
-        Log.i("query",queryString);
+        Log.i("query", queryString);
         query = new SunshineQuery();
         query.addFieldValue("title", queryString);
         surveyFragment.reloadWithQuery(query);
@@ -264,19 +276,19 @@ public class HomeActivity extends AppCompatActivity implements NavigationView.On
 
     @Override
     public boolean onQueryTextChange(String newText) {
-        Log.i("query",newText);
+        Log.i("query", newText);
         return false;
     }
 
     @Override
     public boolean onMenuItemActionExpand(MenuItem item) {
-        Log.i("expand","expand");
+        Log.i("expand", "expand");
         return true;
     }
 
     @Override
     public boolean onMenuItemActionCollapse(MenuItem item) {
-        Log.i("asdf","asdf");
+        Log.i("asdf", "asdf");
         query = null;
         surveyFragment.reloadWithQuery(query);
         discussionFragment.reloadWithQuery(query);
@@ -287,4 +299,141 @@ public class HomeActivity extends AppCompatActivity implements NavigationView.On
     //endregion
 
 
+    private void zoomImageFromThumb(final View thumbView, int imageResId) {
+        // If there's an animation in progress, cancel it
+        // immediately and proceed with this one.
+        if (mCurrentAnimator != null) {
+            mCurrentAnimator.cancel();
+        }
+
+        // Load the high-resolution "zoomed-in" image.
+        final ImageView expandedImageView = (ImageView) findViewById(
+                R.id.expanded_image);
+        expandedImageView.setImageResource(imageResId);
+
+        // Calculate the starting and ending bounds for the zoomed-in image.
+        // This step involves lots of math. Yay, math.
+        final Rect startBounds = new Rect();
+        final Rect finalBounds = new Rect();
+        final Point globalOffset = new Point();
+
+        // The start bounds are the global visible rectangle of the thumbnail,
+        // and the final bounds are the global visible rectangle of the container
+        // view. Also set the container view's offset as the origin for the
+        // bounds, since that's the origin for the positioning animation
+        // properties (X, Y).
+        thumbView.getGlobalVisibleRect(startBounds);
+        findViewById(R.id.container)
+                .getGlobalVisibleRect(finalBounds, globalOffset);
+        startBounds.offset(-globalOffset.x, -globalOffset.y);
+        finalBounds.offset(-globalOffset.x, -globalOffset.y);
+
+        // Adjust the start bounds to be the same aspect ratio as the final
+        // bounds using the "center crop" technique. This prevents undesirable
+        // stretching during the animation. Also calculate the start scaling
+        // factor (the end scaling factor is always 1.0).
+        float startScale;
+        if ((float) finalBounds.width() / finalBounds.height()
+                > (float) startBounds.width() / startBounds.height()) {
+            // Extend start bounds horizontally
+            startScale = (float) startBounds.height() / finalBounds.height();
+            float startWidth = startScale * finalBounds.width();
+            float deltaWidth = (startWidth - startBounds.width()) / 2;
+            startBounds.left -= deltaWidth;
+            startBounds.right += deltaWidth;
+        } else {
+            // Extend start bounds vertically
+            startScale = (float) startBounds.width() / finalBounds.width();
+            float startHeight = startScale * finalBounds.height();
+            float deltaHeight = (startHeight - startBounds.height()) / 2;
+            startBounds.top -= deltaHeight;
+            startBounds.bottom += deltaHeight;
+        }
+
+        // Hide the thumbnail and show the zoomed-in view. When the animation
+        // begins, it will position the zoomed-in view in the place of the
+        // thumbnail.
+        thumbView.setAlpha(0f);
+        expandedImageView.setVisibility(View.VISIBLE);
+
+        // Set the pivot point for SCALE_X and SCALE_Y transformations
+        // to the top-left corner of the zoomed-in view (the default
+        // is the center of the view).
+        expandedImageView.setPivotX(0f);
+        expandedImageView.setPivotY(0f);
+
+        // Construct and run the parallel animation of the four translation and
+        // scale properties (X, Y, SCALE_X, and SCALE_Y).
+        AnimatorSet set = new AnimatorSet();
+        set
+                .play(ObjectAnimator.ofFloat(expandedImageView, View.X,
+                        startBounds.left, finalBounds.left))
+                .with(ObjectAnimator.ofFloat(expandedImageView, View.Y,
+                        startBounds.top, finalBounds.top))
+                .with(ObjectAnimator.ofFloat(expandedImageView, View.SCALE_X,
+                        startScale, 1f)).with(ObjectAnimator.ofFloat(expandedImageView,
+                View.SCALE_Y, startScale, 1f));
+        set.setDuration(mShortAnimationDuration);
+        set.setInterpolator(new DecelerateInterpolator());
+        set.addListener(new AnimatorListenerAdapter() {
+            @Override
+            public void onAnimationEnd(Animator animation) {
+                mCurrentAnimator = null;
+            }
+
+            @Override
+            public void onAnimationCancel(Animator animation) {
+                mCurrentAnimator = null;
+            }
+        });
+        set.start();
+        mCurrentAnimator = set;
+
+        // Upon clicking the zoomed-in image, it should zoom back down
+        // to the original bounds and show the thumbnail instead of
+        // the expanded image.
+        final float startScaleFinal = startScale;
+        expandedImageView.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                if (mCurrentAnimator != null) {
+                    mCurrentAnimator.cancel();
+                }
+
+                // Animate the four positioning/sizing properties in parallel,
+                // back to their original values.
+                AnimatorSet set = new AnimatorSet();
+                set.play(ObjectAnimator
+                        .ofFloat(expandedImageView, View.X, startBounds.left))
+                        .with(ObjectAnimator
+                                .ofFloat(expandedImageView,
+                                        View.Y, startBounds.top))
+                        .with(ObjectAnimator
+                                .ofFloat(expandedImageView,
+                                        View.SCALE_X, startScaleFinal))
+                        .with(ObjectAnimator
+                                .ofFloat(expandedImageView,
+                                        View.SCALE_Y, startScaleFinal));
+                set.setDuration(mShortAnimationDuration);
+                set.setInterpolator(new DecelerateInterpolator());
+                set.addListener(new AnimatorListenerAdapter() {
+                    @Override
+                    public void onAnimationEnd(Animator animation) {
+                        thumbView.setAlpha(1f);
+                        expandedImageView.setVisibility(View.GONE);
+                        mCurrentAnimator = null;
+                    }
+
+                    @Override
+                    public void onAnimationCancel(Animator animation) {
+                        thumbView.setAlpha(1f);
+                        expandedImageView.setVisibility(View.GONE);
+                        mCurrentAnimator = null;
+                    }
+                });
+                set.start();
+                mCurrentAnimator = set;
+            }
+        });
+    }
 }
--- app/src/main/java/com/kcumendigital/democratic/ShareActivity.java
+++ app/src/main/java/com/kcumendigital/democratic/ShareActivity.java
@@ -1,18 +1,13 @@
 package com.kcumendigital.democratic;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-
+import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
-
-
-
 import android.view.View;
 import android.widget.Button;
 
 import com.kcumendigital.democratic.library.Techniques;
 import com.kcumendigital.democratic.library.YoYo;
-import com.kcumendigital.democratic.library.attention.TadaAnimator;
 
 
 public class ShareActivity extends AppCompatActivity implements View.OnClickListener {
